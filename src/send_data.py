4d7947cffc2021a22064a789a15563d9a254e8de
joe-sacco <joesacco@me.com>
2022-09-12 15:10:44 +0900

    feat: Add send data

---
+++ src/send_data.py
@@ -0,0 +1,47 @@
+from ast import alias
+from xmlrpc.client import boolean
+from sensor_temp import getTemp
+from sensor_motion import getMotion 
+from alarm import alarm_on
+from button import button_pressed
+import requests
+import json
+import time
+
+
+URL = 'http://127.0.0.1:8000'
+
+## SEND DATA OUT
+
+def create_sensor_data_dict():
+    try:
+        sensor_temp = getTemp()
+        temp_c = round(sensor_temp["temperature_c"], 1)
+        temp_f = round(sensor_temp["temperature_f"], 1)
+        humidity = sensor_temp["humidity"]
+        motion = getMotion()
+        alarm = alarm_on()
+        button = button_pressed()
+
+        sensor_data_out = {
+            'temperature_c': temp_c,
+            'temperature_f': temp_f,
+            'humidity': humidity,
+            'motion': motion,
+            'alarm': alarm,
+            'button': button
+        }
+        print(sensor_data_out)
+        return sensor_data_out
+    except:
+        pass
+
+## LOOP DATA SEND OUT
+
+
+
+while True:
+    # create_sensor_data_dict()
+    req = requests.post(f'{URL}/sensor_data', json = create_sensor_data_dict())
+    print(req.text)
+    time.sleep(2)
