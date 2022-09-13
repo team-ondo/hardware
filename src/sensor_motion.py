85aa8226d6bb828209ef16ec465d044bf800655e
joe-sacco <joesacco@me.com>
2022-09-13 11:07:19 +0900

    fix: Remove sleep from motion sensor

--- src/sensor_motion.py
+++ src/sensor_motion.py
@@ -7,24 +7,21 @@ pir = 16
 GPIO.setup(pir, GPIO.IN)
 
 print('Initializing Sensor...')
-sleep(2) # allow sensor time to intialize
-print("Let's do this")
 
 def getMotion():
 
     try:
         motion = GPIO.input(pir)
         if motion:
-            # print("MOTION DETECTED")
+            # when motion detected
             return True
         
         elif not motion:
-            # print("<no motion>")
+            # when motion not detected
             return False
 
     except KeyboardInterrupt:
         GPIO.cleanup() #reset GPIO
 
-while True:
+if __name__ == "__main__":
     print(getMotion())
-    sleep(2)
